7d9a6435c37c924440462df32b9909e59913710d
Truong Hoang <truong@codelynx.io>
2019-07-01 16:44:50 +0700

    fix: fix profile

--- client/pages/Profile/index.vue
+++ client/pages/Profile/index.vue
@@ -5,7 +5,6 @@
         <el-col :span="22">
           <el-card :body-style="{ padding: '5px' }">
             <img src="~/assets/images/student.png" class="image">
-            <!--<img :src="imageData" class="image">-->
             <div style="padding: 14px; text-align: center; font-size: 16px; font-weight: bolder">
               <span>Trần Ngọc Quốc</span>
             </div>
@@ -20,61 +19,62 @@
           <el-card :body-style="{ padding: '25px' }">
             <el-form ref="form" :model="form" label-width="120px" label-position="left">
               <el-form-item label="Họ và tên" style="margin-bottom: 10px">
-                <el-input v-model="form.name" readonly="true" placeholder="Trần Ngọc Quốc"></el-input>
+                <el-input v-model="form.name" :readonly="true" placeholder="Trần Ngọc Quốc"></el-input>
               </el-form-item>
 
               <el-form :inline="true" label-position="left" style="margin-bottom: 10px">
                 <el-form-item label="Lớp" label-width="120px" style="margin-right: 36px">
-                  <el-input v-model="form.class" readonly="true" placeholder="10 Toán" style="width: 250px"></el-input>
+                  <el-input
+                    v-model="form.class"
+                    :readonly="true"
+                    placeholder="10 Toán"
+                    style="width: 250px"
+                  ></el-input>
                 </el-form-item>
                 <el-form-item label="Giới tính" label-width="100px" style="margin-right: 0px">
                   <el-radio-group style="width: 200px" v-model="form.radio1">
-                    <el-radio
-                      border
-                      :label="1"
-                      style="margin-right: 2px; width: 94px"
-                    >Nam</el-radio>
-                    <el-radio border :label="2" style="width: 94px">Nữ</el-radio>
+                    <el-radio disabled border :label="1" style="margin-right: 2px; width: 94px">Nam</el-radio>
+                    <el-radio disabled border :label="2" style="width: 94px">Nữ</el-radio>
                   </el-radio-group>
                 </el-form-item>
               </el-form>
 
               <el-form :inline="true" label-position="left" style="margin-bottom: 10px">
-                <!--<el-form-item label="Năm sinh"  label-width="120px" style="margin-right: 36px">
-                  <el-date-picker
-                    type="date"
-                    placeholder="2019-01-28"
-                    v-model="form.dateOfBirth"
-                    readonly="true"
-                    style="width: 250px;"
-                  ></el-date-picker>
-                </el-form-item>-->
-
                 <el-form-item label="Năm sinh" label-width="120px" style="margin-right: 36px">
                   <el-input
                     v-model="form.dateOfBirth"
-                    placeholder=""
+                    placeholder
                     style="width: 250px"
-                    readonly="true"
+                    :readonly="true"
                   ></el-input>
-                </el-form-item>  
+                </el-form-item>
 
                 <el-form-item label="Nơi sinh" label-width="100px" style="margin-right: 0px">
                   <el-input
                     v-model="form.placeOfBirth"
                     placeholder="Nha Trang - Khánh Hòa"
                     style="width: 200px"
-                    readonly="true"
+                    :readonly="true"
                   ></el-input>
                 </el-form-item>
               </el-form>
 
               <el-form :inline="true" label-position="left" style="margin-bottom: 10px">
                 <el-form-item label="Quốc tịch" label-width="120px" style="margin-right: 36px">
-                  <el-input v-model="form.nationality" readonly="true" placeholder="Việt Nam" style="width: 250px"></el-input>
+                  <el-input
+                    v-model="form.nationality"
+                    :readonly="true"
+                    placeholder="Việt Nam"
+                    style="width: 250px"
+                  ></el-input>
                 </el-form-item>
                 <el-form-item label="Dân tộc" label-width="100px" style="margin-right: 0px">
-                  <el-input v-model="form.people" readonly="true" placeholder="Kinh" style="width: 200px"></el-input>
+                  <el-input
+                    v-model="form.people"
+                    :readonly="true"
+                    placeholder="Kinh"
+                    style="width: 200px"
+                  ></el-input>
                 </el-form-item>
               </el-form>
 
@@ -83,27 +83,38 @@
                   <el-input v-model="form.phone" placeholder="0905417845" style="width: 250px"></el-input>
                 </el-form-item>
                 <el-form-item label="CMND" label-width="100px" style="margin-right: 0px">
-                  <el-input v-model="form.identity_card" placeholder="123456789" style="width: 200px"></el-input>
+                  <el-input
+                    v-model="form.identity_card"
+                    placeholder="123456789"
+                    style="width: 200px"
+                    resize="none"
+                  ></el-input>
                 </el-form-item>
               </el-form>
 
               <el-form-item label="Địa chỉ" style="margin-bottom: 10px">
                 <el-input
+                  :readonly="true"
                   type="textarea"
                   v-model="form.address"
+                  resize="none"
                   placeholder="22/ Phan Văn Trị/ Cầu Đá/ Vĩnh Nguyên/ Nha Trang/ Khánh Hòa "
                 ></el-input>
               </el-form-item>
 
               <el-form-item label="Ghi chú">
-                <el-input type="textarea" v-model="form.notice" placeholder></el-input>
+                <el-input
+                  :readonly="true"
+                  resize="none"
+                  type="textarea"
+                  v-model="form.notice"
+                  placeholder
+                ></el-input>
               </el-form-item>
             </el-form>
           </el-card>
         </el-col>
       </el-row>
-      <!--<el-button type="primary">Sửa đổi</el-button>
-      <el-button @click="updateStudent" type="primary">Cập nhập</el-button>-->
     </div>
   </div>
 </template>
@@ -116,15 +127,6 @@ export default {
     return {
       currentDate: new Date(),
       form: {
-        /*name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: "",
-        radio: "Nam"*/
         name: "Trần Ngọc Quốc",
         class: "12 Toán",
         radio1: 1,
@@ -160,8 +162,8 @@ export default {
   .user_avatar {
     width: 30%;
     .image {
-      width: 300px;
-      height: 300px;
+      max-width: 200px;
+      max-height: 300px;
       border: 1px solid;
       border-radius: 150px;
       display: block;

b657cda006fb43df5f8327e84a4a877232a648c1
Truong Hoang <truong@codelynx.io>
2019-07-01 12:07:22 +0700

    refactor: image name

---
+++ client/components/TimeTable/ClassHour.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hour">Tiết {{number}}</div>
+</template>
+
+<script>
+export default {
+  name: "ClassHour",
+  props: ["number"]
+};
+</script>
+
+<style lang="scss" scoped>
+.hour {
+  width: 100%;
+
+  // box-sizing: border-box;
+  // &:not(:first-child) {
+  //   border-top: 0.3px solid $primary-color;
+  // }
+}
+</style>
--- client/pages/Notification/index.vue
+++ client/pages/Notification/index.vue
@@ -1,103 +1,3 @@
-<!--<template>
-  <div class="notification" :data="notification_data">
-    <div class="notificationBar" v-on:click="changeStatus()">
-      <img class="notification-img" prop="noti_img" :src="imgName">
-      <h1 class="notification-content" prop="noti_content">{{ content }}</h1>
-      <div class="notification-time" prop="noti_time">{{ time }}</div>
-    </div>
-  </div>
-</template> -->
-
-<!--<script>
-export default {
-  name: "Notification",
-  computed: {
-    notification_data: function() {
-      const subject = {
-        imgName: "~/assets/images/red-square.png",
-        content: "Welcome to QT_EDU",
-        time: new Date().toLocaleDateString("en-GB")
-      };
-      let subs = [];
-      for (let i = 0; i < 5; i++) {
-        subs.push(subject);
-      }
-
-      const data = subs.map(s => {
-        return {
-          noti_img: s.imgName,
-          noti_content: s.content,
-          noti_time: s.time
-        };
-      });
-
-      return data;
-    }
-  },
-  data() {
-    return {
-      imgName: require("~/assets/images/red-square.png"),
-      content: "This is the first notification on this web: Welcome to QT_EDU",
-      time: new Date().toLocaleDateString("en-GB")
-    };
-  },
-  methods: {
-    getImgUrl(pic) {
-      return require("~/assets/images/" + pic);
-    },
-    changeStatus: function() {
-      this.imgName = require("~/assets/images/green-square.png");
-    }
-  }
-};
-</script> -->
-
-<!--<style lang="scss" scoped>
-.notification {
-  background: #fff;
-  .notificationBar {
-    background-color: #fff;
-    height: 50px;
-    position: relative;
-    display: flex;
-    cursor: pointer;
-    border: 1px solid plum;
-    border-radius: 7px;
-    width: 90%;
-    margin-left: 20px;
-
-    .notification-img {
-      position: relative;
-      width: 49px;
-      height: 49px;
-      border-radius: 7px;
-    }
-
-    .notification-content {
-      margin: auto;
-      margin-left: 0px;
-      margin-right: 0px;
-      padding-top: 10px;
-      padding-left: 10px;
-      width: 80%;
-      height: 100%;
-      font-size: 16px;
-      font-weight: bolder;
-      background: rgb(240, 240, 240);
-    }
-    .notification-time {
-      padding-top: 10px;
-      height: 100%;
-      font-size: 16px;
-      font-weight: bolder;
-      border-left: 1px solid plum;
-      text-align: center;
-      padding-left: 30px;
-    }
-  }
-}
-</style> -->
-
 <template>
   <div>
     <el-card class="box-card" style="width: 80%; margin: auto; margin-bottom: 10px;">
@@ -121,7 +21,7 @@ export default {
 
     <el-card class="box-card" style="width: 80%; margin: auto; margin-bottom: 10px;">
       <div slot="header" class="clearfix" style="padding: auto">
-        <img class="notification-img" prop="noti_img" src="~/assets/images/non-bell.png">
+        <img class="notification-img" prop="noti_img" :src="nonBell">
         <span style="font-weight: bold; text-transform: uppercase">Thông báo từ QT-EDU</span>
         <p style="float: right; color: blue" type="text">03-06-2019</p>
       </div>
@@ -136,7 +36,7 @@ export default {
     </el-card>
     <el-card class="box-card" style="width: 80%; margin: auto; margin-bottom: 10px;">
       <div slot="header" class="clearfix" style="padding: auto">
-        <img class="notification-img" prop="noti_img" src="~/assets/images/non-bell.png">
+        <img class="notification-img" prop="noti_img" :src="nonBell">
         <span style="font-weight: bold; text-transform: uppercase">Thông báo từ QT-EDU</span>
         <p style="float: right; color: blue" type="text">25-05-2019</p>
       </div>
@@ -152,7 +52,7 @@ export default {
 
     <el-card class="box-card" style="width: 80%; margin: auto; margin-bottom: 10px;">
       <div slot="header" class="clearfix" style="padding: auto">
-        <img class="notification-img" prop="noti_img" src="~/assets/images/non-bell.png">
+        <img class="notification-img" prop="noti_img" :src="nonBell">
         <span style="font-weight: bold; text-transform: uppercase">Thông báo từ QT-EDU</span>
         <p style="float: right; color: blue" type="text">20-05-2019</p>
       </div>
@@ -174,7 +74,8 @@ export default {
   name: "notification",
   data() {
     return {
-      imgName: require("~/assets/images/ring-bell.png")
+      imgName: require("~/assets/images/ring-bell.png"),
+      nonBell: require("~/assets/images/non-bell.png")
     };
   },
   methods: {
